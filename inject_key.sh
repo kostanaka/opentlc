814a48165469622c647ba7d15ccd55e8d3f389e9
Kosuke Tanaka <ktanaka@ktanaka.osaka.redhat.com>
2017-06-07 19:57:07 +0859

    changed

--- inject_key.sh
+++ inject_key.sh
@@ -9,4 +9,7 @@ fi
 export PASS='r3dh4t1!'
 export SSH_ASKPASS=$0
 
-ssh-copy-id -i /root/.ssh/id_rsa.pub ansible1.example.com
+#ssh-copy-id -i /root/.ssh/id_rsa.pub ansible1.example.com
+
+ssh ansible1.example.com ls /
+
