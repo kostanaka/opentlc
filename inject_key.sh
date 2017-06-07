7c5acd82b589f2a01f090ac85fd8ae6f3ca958ad
Kosuke Tanaka <ktanaka@ktanaka.osaka.redhat.com>
2017-06-07 20:28:27 +0859

    replaced

--- inject_key.sh
+++ inject_key.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/bash
-#
-if [ x"$PASS" == x ]; then
-    echo PASSWORD > /dev/tty
-    echo $PASS
-    exit 0
+if [ $(whoami) != root ];then
+	echo "you have to be root."
+	exit 1
 fi
-
-export PASS='r3dh4t1!'
-export SSH_ASKPASS=$0
-
-#ssh-copy-id -i /root/.ssh/id_rsa.pub ansible1.example.com
-
-ssh ansible1.example.com ls /
-
+echo "copying key to ansible tower host."
+ssh-copy-id ansible1.example.com
