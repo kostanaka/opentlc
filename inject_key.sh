8bc15eb529175ffb26def45cd14c075d8e19273c
Kosuke Tanaka <ktanaka@ktanaka.osaka.redhat.com>
2017-06-08 12:53:22 +0859

    changed

--- inject_key.sh
+++ inject_key.sh
@@ -3,5 +3,10 @@ if [ $(whoami) != root ];then
 	echo "you have to be root."
 	exit 1
 fi
+PASS="r3dh4t1!"
 echo "copying key to ansible tower host."
-ssh-copy-id ansible1.example.com
+expect -f - << EXPECT
+spawn ssh-copy-id ansible1.example.com
+expect "password:"
+send $PASS
+EXPECT
