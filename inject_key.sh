64ce240ed4337e0c5d1cb958f05e18366087197e
Kosuke Tanaka <ktanaka@ktanaka.osaka.redhat.com>
2017-06-08 19:24:08 +0859

    modified

--- inject_key.sh
+++ inject_key.sh
@@ -8,15 +8,20 @@ PASS="r3dh4t1!"
 
 expect -f - << EXPECT
 spawn ssh-copy-id root@ansible1.example.com
-expect "password: " {
-	send "$PASS\r"
-} "already exist" {
-	exit 0
+proc_login {} {
+  expect timeout {
+    exit 1
+  } "continue connecting (yes/no)? " {
+    send "yes\r"
+    proc_login
+  } "password: " {
+    send "$PASS\r"
+    proc_login
+  } "already exist" {
+    exit 0
+  } "added: 1" {
+    exit 0
+  }
 }
-
-expect timeout {
-       exit 1
-} "added: 1" {
-       exit 0
-}
+proc_login
 EXPECT
